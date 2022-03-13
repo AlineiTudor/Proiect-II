f7d6804a40b75dde2618d6032005fa5dbcd217ca
Alinei_Tudor <tudor0008@gmail.com>
2022-03-13 12:28:23 +0200

    primul commit

---
+++ Car_game/Car_game/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿
+namespace Car_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.masina = new System.Windows.Forms.PictureBox();
+            this.inamic1 = new System.Windows.Forms.PictureBox();
+            this.inamic3 = new System.Windows.Forms.PictureBox();
+            this.inamic2 = new System.Windows.Forms.PictureBox();
+            this.gameOver = new System.Windows.Forms.Label();
+            this.banut1 = new System.Windows.Forms.PictureBox();
+            this.banut4 = new System.Windows.Forms.PictureBox();
+            this.banut5 = new System.Windows.Forms.PictureBox();
+            this.banut3 = new System.Windows.Forms.PictureBox();
+            this.banut2 = new System.Windows.Forms.PictureBox();
+            this.scor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masina)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inamic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inamic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inamic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banut1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banut4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banut5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banut3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banut2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(220, -16);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 90);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(220, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(220, 253);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 90);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.Location = new System.Drawing.Point(220, 393);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(30, 90);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Location = new System.Drawing.Point(220, 518);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(30, 90);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.Location = new System.Drawing.Point(-13, -16);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(23, 599);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.White;
+            this.pictureBox7.Location = new System.Drawing.Point(471, -16);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(23, 599);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tag = "";
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // masina
+            // 
+            this.masina.Image = ((System.Drawing.Image)(resources.GetObject("masina.Image")));
+            this.masina.Location = new System.Drawing.Point(149, 377);
+            this.masina.Name = "masina";
+            this.masina.Size = new System.Drawing.Size(65, 62);
+            this.masina.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.masina.TabIndex = 8;
+            this.masina.TabStop = false;
+            // 
+            // inamic1
+            // 
+            this.inamic1.Image = global::Car_game.Properties.Resources.inamic_premium;
+            this.inamic1.Location = new System.Drawing.Point(28, 39);
+            this.inamic1.Name = "inamic1";
+            this.inamic1.Size = new System.Drawing.Size(71, 66);
+            this.inamic1.TabIndex = 9;
+            this.inamic1.TabStop = false;
+            // 
+            // inamic3
+            // 
+            this.inamic3.Image = global::Car_game.Properties.Resources.inamic_premium;
+            this.inamic3.Location = new System.Drawing.Point(245, 104);
+            this.inamic3.Name = "inamic3";
+            this.inamic3.Size = new System.Drawing.Size(71, 66);
+            this.inamic3.TabIndex = 10;
+            this.inamic3.TabStop = false;
+            // 
+            // inamic2
+            // 
+            this.inamic2.Image = global::Car_game.Properties.Resources.inamic_premium;
+            this.inamic2.Location = new System.Drawing.Point(336, 39);
+            this.inamic2.Name = "inamic2";
+            this.inamic2.Size = new System.Drawing.Size(71, 66);
+            this.inamic2.TabIndex = 11;
+            this.inamic2.TabStop = false;
+            // 
+            // gameOver
+            // 
+            this.gameOver.AutoSize = true;
+            this.gameOver.BackColor = System.Drawing.Color.Yellow;
+            this.gameOver.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gameOver.ForeColor = System.Drawing.Color.Red;
+            this.gameOver.Location = new System.Drawing.Point(132, 185);
+            this.gameOver.Name = "gameOver";
+            this.gameOver.Size = new System.Drawing.Size(202, 45);
+            this.gameOver.TabIndex = 12;
+            this.gameOver.Text = "GAME OVER";
+            // 
+            // banut1
+            // 
+            this.banut1.Image = global::Car_game.Properties.Resources.Banut;
+            this.banut1.Location = new System.Drawing.Point(75, 277);
+            this.banut1.Name = "banut1";
+            this.banut1.Size = new System.Drawing.Size(46, 46);
+            this.banut1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.banut1.TabIndex = 13;
+            this.banut1.TabStop = false;
+            // 
+            // banut4
+            // 
+            this.banut4.Image = global::Car_game.Properties.Resources.Banut;
+            this.banut4.Location = new System.Drawing.Point(218, 253);
+            this.banut4.Name = "banut4";
+            this.banut4.Size = new System.Drawing.Size(46, 46);
+            this.banut4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.banut4.TabIndex = 14;
+            this.banut4.TabStop = false;
+            // 
+            // banut5
+            // 
+            this.banut5.Image = global::Car_game.Properties.Resources.Banut;
+            this.banut5.Location = new System.Drawing.Point(320, 277);
+            this.banut5.Name = "banut5";
+            this.banut5.Size = new System.Drawing.Size(46, 46);
+            this.banut5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.banut5.TabIndex = 15;
+            this.banut5.TabStop = false;
+            // 
+            // banut3
+            // 
+            this.banut3.Image = global::Car_game.Properties.Resources.Banut;
+            this.banut3.Location = new System.Drawing.Point(132, 104);
+            this.banut3.Name = "banut3";
+            this.banut3.Size = new System.Drawing.Size(46, 46);
+            this.banut3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.banut3.TabIndex = 16;
+            this.banut3.TabStop = false;
+            // 
+            // banut2
+            // 
+            this.banut2.Image = global::Car_game.Properties.Resources.Banut;
+            this.banut2.Location = new System.Drawing.Point(95, 185);
+            this.banut2.Name = "banut2";
+            this.banut2.Size = new System.Drawing.Size(46, 46);
+            this.banut2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.banut2.TabIndex = 17;
+            this.banut2.TabStop = false;
+            // 
+            // scor
+            // 
+            this.scor.AutoSize = true;
+            this.scor.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.scor.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.scor.Location = new System.Drawing.Point(12, 8);
+            this.scor.Name = "scor";
+            this.scor.Size = new System.Drawing.Size(87, 28);
+            this.scor.TabIndex = 18;
+            this.scor.Text = "Banuti: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(482, 553);
+            this.Controls.Add(this.scor);
+            this.Controls.Add(this.banut2);
+            this.Controls.Add(this.banut3);
+            this.Controls.Add(this.banut5);
+            this.Controls.Add(this.banut4);
+            this.Controls.Add(this.banut1);
+            this.Controls.Add(this.gameOver);
+            this.Controls.Add(this.inamic2);
+            this.Controls.Add(this.inamic3);
+            this.Controls.Add(this.inamic1);
+            this.Controls.Add(this.masina);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masina)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inamic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inamic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inamic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banut1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banut4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banut5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banut3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.banut2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox masina;
+        private System.Windows.Forms.PictureBox inamic1;
+        private System.Windows.Forms.PictureBox inamic3;
+        private System.Windows.Forms.PictureBox inamic2;
+        private System.Windows.Forms.Label gameOver;
+        private System.Windows.Forms.PictureBox banut1;
+        private System.Windows.Forms.PictureBox banut4;
+        private System.Windows.Forms.PictureBox banut5;
+        private System.Windows.Forms.PictureBox banut3;
+        private System.Windows.Forms.PictureBox banut2;
+        private System.Windows.Forms.Label scor;
+    }
+}
+
---
+++ Car_game/Car_game/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_game
+{
+    public partial class Form1 : Form
+    {
+        int viteza_joc = 5;
+        int banutiColectati = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            gameOver.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e){}
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveline(viteza_joc);
+            inamic(viteza_joc);
+            aiPierdut();
+            miscareBanuti(viteza_joc);
+            strangeBanuti();
+        }
+        void moveline(int speed)
+        {
+            if (pictureBox1.Top >= this.ClientRectangle.Height)
+            { pictureBox1.Top = -pictureBox1.Height; }
+            else { pictureBox1.Top += speed; }
+
+            if (pictureBox2.Top >= this.ClientRectangle.Height)
+            { pictureBox2.Top = -pictureBox2.Height; }
+            else { pictureBox2.Top += speed; }
+
+            if (pictureBox3.Top >= this.ClientRectangle.Height)
+            { pictureBox3.Top = -pictureBox3.Height; }
+            else { pictureBox3.Top += speed; }
+
+            if (pictureBox4.Top >= this.ClientRectangle.Height)
+            { pictureBox4.Top = -pictureBox4.Height; }
+            else { pictureBox4.Top += speed; }
+
+            if (pictureBox5.Top >= this.ClientRectangle.Height)
+            { pictureBox5.Top = -pictureBox5.Height; ; }
+            else { pictureBox5.Top += speed; }
+        }
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int spdLR = 8;
+            if (e.KeyCode == Keys.Left)
+            {
+                if (masina.Left > pictureBox7.Width/2)
+                { masina.Left += -spdLR; }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (masina.Left < this.ClientRectangle.Width-masina.Width-pictureBox7.Width/2) 
+                { masina.Left += spdLR; }
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                if (viteza_joc <= 15)
+                {
+                    viteza_joc++;
+                }
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (viteza_joc >=5)
+                {
+                    viteza_joc--;
+                }
+            }
+        }
+
+        Random r = new Random();
+        int poz_inamic_x;
+        void inamic(int speed)
+        {
+            if (inamic1.Top >= ClientRectangle.Height)
+            { 
+                inamic1.Top = -inamic1.Height;
+                poz_inamic_x = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2-inamic1.Width);
+                inamic1.Left = poz_inamic_x;
+            }
+            else{ inamic1.Top += speed; }
+
+            if (inamic2.Top >= ClientRectangle.Height)
+            {
+                inamic2.Top = -inamic2.Height;
+                poz_inamic_x = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - inamic2.Width);
+                inamic2.Left = poz_inamic_x;
+            }
+            else { inamic2.Top += speed; }
+
+            if (inamic3.Top >= ClientRectangle.Height)
+            {
+                inamic3.Top = -inamic3.Height;
+                poz_inamic_x = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - inamic3.Width);
+                inamic3.Left = poz_inamic_x;
+            }
+            else { inamic3.Top += speed; }
+
+        }
+
+        void aiPierdut()
+        {
+            if (masina.Bounds.IntersectsWith(inamic1.Bounds))
+            {
+                timer1.Enabled = false;
+                gameOver.Visible = true;
+            }
+            if (masina.Bounds.IntersectsWith(inamic2.Bounds))
+            {
+                timer1.Enabled = false;
+                gameOver.Visible = true;
+            }
+            if (masina.Bounds.IntersectsWith(inamic3.Bounds))
+            {
+                timer1.Enabled = false;
+                gameOver.Visible = true;
+            }
+        }
+
+        int poz_banut;
+        void miscareBanuti(int speed) {
+            if (banut1.Top >= ClientRectangle.Height)
+            {
+                banut1.Top = -banut1.Height;
+                poz_banut = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - banut1.Width);
+                banut1.Left = poz_banut;
+            }
+            else { banut1.Top += speed; }
+
+            if (banut2.Top >= ClientRectangle.Height)
+            {
+                banut2.Top = -banut2.Height;
+                poz_banut = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - banut2.Width);
+                banut2.Left = poz_banut;
+            }
+            else { banut2.Top += speed; }
+            if (banut3.Top >= ClientRectangle.Height)
+            {
+                banut3.Top = -banut3.Height;
+                poz_banut = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - banut3.Width);
+                banut3.Left = poz_banut;
+            }
+            else { banut3.Top += speed; }
+            if (banut4.Top >= ClientRectangle.Height)
+            {
+                banut4.Top = -banut4.Height;
+                poz_banut = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - banut4.Width);
+                banut4.Left = poz_banut;
+            }
+            else { banut4.Top += speed; }
+            if (banut5.Top >= ClientRectangle.Height)
+            {
+                banut5.Top = -banut5.Height;
+                poz_banut = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - banut5.Width);
+                banut5.Left = poz_banut;
+            }
+            else { banut5.Top += speed; }
+        }
+
+        void strangeBanuti()
+        {
+            if (masina.Bounds.IntersectsWith(banut1.Bounds))
+            {
+                banut1.Top = -banut1.Height;
+                poz_banut = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - banut1.Width);
+                banut1.Left = poz_banut;
+                banutiColectati++;
+                scor.Text = "Banuti: " + banutiColectati.ToString();
+            }
+            if (masina.Bounds.IntersectsWith(banut2.Bounds))
+            {
+                banut2.Top = -banut2.Height;
+                poz_banut = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - banut2.Width);
+                banut2.Left = poz_banut;
+                banutiColectati++;
+                scor.Text = "Banuti: " + banutiColectati.ToString();
+            }
+            if (masina.Bounds.IntersectsWith(banut3.Bounds))
+            {
+                banut3.Top = -banut3.Height;
+                poz_banut = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - banut3.Width);
+                banut3.Left = poz_banut;
+                banutiColectati++;
+                scor.Text = "Banuti: " + banutiColectati.ToString();
+            }
+            if (masina.Bounds.IntersectsWith(banut4.Bounds))
+            {
+                banut4.Top = -banut4.Height;
+                poz_banut = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - banut4.Width);
+                banut4.Left = poz_banut;
+                banutiColectati++;
+                scor.Text = "Banuti: " + banutiColectati.ToString();
+            }
+            if (masina.Bounds.IntersectsWith(banut5.Bounds))
+            {
+                banut5.Top = -banut5.Height;
+                poz_banut = r.Next(pictureBox6.Width / 2, ClientRectangle.Width - pictureBox7.Width / 2 - banut5.Width);
+                banut5.Left = poz_banut;
+                banutiColectati++;
+                scor.Text = "Banuti: " + banutiColectati.ToString();
+            }
+
+        }
+
+    }
+}
